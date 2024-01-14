697df5a6499ca7d1701cc4e66e9745893459196b
mohamedbashir <mohaedbashir45@gmail.com>
2024-01-14 15:18:05 +0300

    done

---
+++ index.js
@@ -0,0 +1,27 @@
+const fi = (function() {
+    return {
+      libraryMethod: function() {
+        return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0';
+      },
+  
+      each: function(collection, callback) {
+        // Implement the logic for 'each' function here
+      },
+  
+      map: function(collection, callback) {
+        // Implement the logic for 'map' function here
+      },
+  
+      reduce: function(collection, callback, accumulator) {
+        // Implement the logic for 'reduce' function here
+      },
+  
+      functions: function() {
+        // Implement the logic for 'functions' function here
+      },
+    };
+  })();
+  
+  // Example usage:
+  fi.libraryMethod(); // Returns the initial message
+  
